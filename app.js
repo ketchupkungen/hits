0f0314084cc83550048a5fc18af33c8f99e1165b
Bjorn Ohlsson <bjorn.ohlsson@glimworks.com>
2018-02-20 13:24:24 +0100

    Reinstated lines that accidentally got removed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const db = pm(
     host: "127.0.0.1",
     user: "root",
     password: devPassword.unique(), // Edit to YOUR password in the file dev-password.js. Otherwise it will not work.
-    database: "sitepoint", //database: "eia",
+    database: "hits", //database: "hits",
     multipleStatements: true
   }),
   {
@@ -20,6 +20,47 @@ const db = pm(
   }
 );
 
+async function test() {
+  let tables = await db.query('SHOW TABLES');
+  console.log(tables);
+}
+
+async function allUsers() {
+  let users = await db.query('SELECT * FROM users');
+  console.log(users);
+}
+
+async function allMessages() {
+  let messages = await db.query('SELECT * FROM messages');
+  console.log(messages);
+}
+
+async function addUser(email, phone, username, firstname, lastname, password, gender) {
+  let users = await db.query('INSERT INTO users SET?', {
+    email: email,
+    phone: phone,
+    username: username,
+    firstname: firstname,
+    lastname: lastname,
+    password: password,
+    gender: gender
+  });
+  console.log(users);
+}
+
+async function updateUser() {
+
+}
+
+async function addMessage(message) {
+  let messages = await db.query('INSERT INTO messages SET message');
+  console.log(messages);
+}
+
+//addUser("testingThis@gmail.com", "67897098", "Stevo", "Stuart", "Bond", "whatever123", "Man");
+
+
+addMessage("okej");
 // If no error, it says its ok
 db.connect((err) => {
   if(err){
