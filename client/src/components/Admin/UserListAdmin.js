acd006e59bcf1ed43395a0bb577d045e7d4dbcbb
Bjorn Ohlsson <bjorn.ohlsson@glimworks.com>
2018-04-12 20:11:01 +0200

    Some tweaing and styling

--- client/src/components/Admin/UserListAdmin.js
+++ client/src/components/Admin/UserListAdmin.js
@@ -1,67 +1,64 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import { Link } from 'react-router-dom';
-import { getUsers, deleteUser } from '../../actions';
+import { Link } from 'react-router-dom';
+import { getUsers } from '../../actions';
 
-import { Card,CardBody,Button } from 'mdbreact';
-import ImgFallback from 'react-img-fallback'
+//import { ListGroup, ListGroupItem, Button } from 'mdbreact';
+//import ImgFallback from 'react-img-fallback'
 
 class UserContainerAdmin extends Component {
 
-    deletePost = () => {
-        this.props.dispatch(deleteUser(this.props.match.params.id))
-    }
+  /*deletePost = () => {
+      this.props.dispatch(deleteUser(this.props.match.params.id))
+  }*/
 
-    componentWillMount(){
-        this.props.dispatch(getUsers())
-    }
+  componentWillMount(){
+      this.props.dispatch(getUsers())
+  }
 
-    showUsers = (user) =>(
-        user.users ?
-            user.users.map(item => (
-              <div className="col-md-4" key={item._id}>
-                <Card cascade style={{height:'700px',marginBottom:'30px'}}>
-                  <ImgFallback
-                    className="img-fluid"
-                    src={item.image}
-                    alt="user-img"
-                    fallback='https://stroops.com/wp-content/uploads/2016/11/placeholder-profile-male-500x500.png'
-                  >
-                  </ImgFallback>
-                  <CardBody>
-                    <h4>Name: {item.name} {item.lastname}</h4>
-                    <h5>Username: {item.username}</h5>
-                    <p>Email: {item.email}</p>
-                    <p>Phone: {item.phone}</p>
-                    <p>Career: {item.career}</p>
-                    <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <Button color="dark-green">Edit</Button>
-                    <Button onClick={this.deletePost} color="red">Delete</Button>
-                  </CardBody>
-                </Card>
-              </div>
-            ))
-        :null
-    )
+  showUsers = (user) =>(
+    user.users ?
+      user.users.map(item => (
+        <tr key={item._id}>
+          <td><p>{item.name} {item.lastname}</p></td>
+          <td><p>{item.email}</p></td>
+          <td><p>0</p></td>
+          <td><p>no</p></td>
+          <td><p>user</p></td>
+          <td><Link to="/users-admin"><i className="fas fa-edit" color="green"></i></Link></td>
+        </tr>
+      ))
+    :null
+  )
 
 
-    render() {
-        let user = this.props.user;
-        return (
-          <div className="container profile-page">
-            <h4>Current users:</h4>
-            <div className="row">
-
-              {this.showUsers(user)}
-            </div>
-          </div>
-        );
-    }
+  render() {
+    let user = this.props.user;
+    return (
+      <div className="profile-page">
+        <table className="table">
+          <thead>
+            <tr>
+              <th><strong>NAME</strong></th>
+              <th><strong>EMAIL</strong></th>
+              <th><strong>WARNINGS</strong></th>
+              <th><strong>BANNED</strong></th>
+              <th><strong>ROLE</strong></th>
+              <th><strong>EDIT</strong></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.showUsers(user)}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 function mapStateToProps (state) {
-    return{
-        user:state.user
-    }
+  return{
+    user:state.user
+  }
 }
 
 export default connect(mapStateToProps)(UserContainerAdmin)
